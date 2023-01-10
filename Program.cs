10b2cae8d28cf81d8d73dd546c6da7a80b436ac4
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2023-01-10 17:37:10 -0300

    Buscando usuários utilizando pacote Dapper.Contrib

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Blog.Models;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+public class Program
+{
+    private const string CONNECTION_STRING = "Server=localhost,1433;" +
+                                             "Database=Blog;" +
+                                             "User ID=sa;" +
+                                             "Password=H2g@5dT$;" +
+                                             "Trusted_Connection=False;" +
+                                             "TrustServerCertificate=True";
+
+    public static void Main(string[] args)
+    {
+        ReadUsers();
+    }
+
+    public static void ReadUsers()
+    {
+        using(var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            var users = connection.GetAll<User>();
+
+            foreach(var user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
+        }
+    }
+}
